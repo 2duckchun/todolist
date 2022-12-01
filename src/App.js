f2186619d3fbb286f32c2dab9c93353d13372a6d
2DC <kkts9308@gmail.com>
2022-12-01 22:29:09 +0900

    투두리스트 gh-pages 설정

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './reset.css'
 import './App.css';
 import { useState } from 'react'
 
-function App() {
 
+function App() {
   const 할일스토리지 = JSON.parse(localStorage.getItem('todo')) || ['React의 첫 발걸음입니다.']
   const 날짜스토리지 = JSON.parse(localStorage.getItem('day')) || [new Date().toLocaleString('ko-KR')]
 
@@ -23,15 +23,33 @@ function App() {
     set할일입력('')
   }
 
+  const 할일삭제 = (i) => {
+    const copy할일목록 = [...할일목록]
+    const copy날짜 = [...날짜]
+    copy할일목록.splice(i, 1)
+    copy날짜.splice(i, 1)
+    set할일목록(copy할일목록)
+    set날짜(copy날짜)
+    localStorage.setItem('todo', JSON.stringify(copy할일목록))
+    localStorage.setItem('day', JSON.stringify(copy날짜))
+  }
+
+  setTimeout(() => {
+    set제목날짜([new Date().toLocaleString('ko-KR')])
+  }, 1000);
+  
+  const [제목날짜, set제목날짜] = useState([new Date().toLocaleString('ko-KR')])
   const [할일목록, set할일목록] = useState(할일스토리지)
   const [날짜, set날짜] = useState(날짜스토리지)
   const [할일입력, set할일입력] = useState('')
   const [입력날짜, set입력날짜] = useState([new Date().toLocaleString('ko-KR')])
 
+
+
   return (
     <div className="App">
       <div className="cont-main">
-        <h1 className="txt-header">2DC - REACT TODOLIST</h1>
+        <h1 className="txt-header">{제목날짜[0]}</h1>
         <div className="cont-todolist">
           <ul>
             {
@@ -41,9 +59,7 @@ function App() {
                     <p>{할일목록[i]}</p>
                     <div className='cont-control'>
                       <button onClick={(e) => {
-                        const copy할일목록 = [...할일목록]
-                        copy할일목록.splice(i, 1)
-                        set할일목록(copy할일목록)
+                        할일삭제(i)
                       }}>삭제</button>
                       <span>{날짜[i]}</span>
                     </div>
@@ -59,10 +75,13 @@ function App() {
               set입력날짜(new Date().toLocaleString('ko-KR'))
             }} onKeyUp={e => {
               if (e.key === 'Enter') {
-                목록렌더링()
+                할일입력.length <= 3 ? alert('좀 더 적어주세요!') : 목록렌더링()
               };
             }}></input>
-            <button onClick={목록렌더링}>업로드</button>
+            <button className="btn-enter" onClick={ () => {
+              할일입력.length <= 3 ? alert('좀 더 적어주세요!') : 목록렌더링()
+            }
+            }>Enter</button>
         </div>
       </div>
     </div>
